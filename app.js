87ebeb43c9471983ec2ac072e004a7c977097e2c
GORKEM1986 <aslanakbey7106@gmail.com>
2023-01-23 21:04:01 +0100

    son

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+const taxRate = 0.18;
+const shippingPrice = 15;
+const shippingFreePrice = 300;
+
+window.addEventListener("load", () =>{
+    localStorage.setItem("taxRate", taxRate);
+    localStorage.setItem("shippingPrice", shippingPrice);
+    localStorage.setItem("shippingFreePrice", shippingFreePrice);
+});
+
+const productsDiv = document.querySelector(".products");
+
+produc
