1aa3fa85893e43d79a148af387811baa29c56609
GORKEM1986 <aslanakbey7106@gmail.com>
2023-01-25 00:16:01 +0100

    Cart price

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+// const taxRate = 0.18;
+// const shippingPrice = 15;
+// const shippingFreePrice = 300;
 
-const taxRate = 0.18;
-const shippingPrice = 15;
-const shippingFreePrice = 300;
-
-window.addEventListener("load", () =>{
-    localStorage.setItem("taxRate", taxRate);
-    localStorage.setItem("shippingPrice", shippingPrice);
-    localStorage.setItem("shippingFreePrice", shippingFreePrice);
+window.addEventListener("load", () => {
+  // localStorage.setItem("taxRate", taxRate);
+  // localStorage.setItem("shippingPrice", shippingPrice);
+  // localStorage.setItem("shippingFreePrice", shippingFreePrice);
+  calculateCartPrice();
 });
 
 const productsDiv = document.querySelector(".products");
 
-produc
+productsDiv.addEventListener("click", (e) => {
+  // alert(e.target.tagName);
+  // alert(e.eventTarget.className)
+  if (e.target.className == "fa-solid fa-minus") {
+    alert("minus btn clicked");
+    calculateProductPrice();
+    calculateCartPrice();
+  } else if (e.target.classList.contains("fa-plus")) {
+    alert("plus btn clicked");
+    calculateProductPrice();
+    calculateCartPrice();
+  } else if (e.target.getAttribute("class") == "remove-product") {
+    alert("remove btn clicked");
+    calculateCartPrice();
+  } else {
+    alert("other element clicked");
+  }
+});
+
+const calculateProductPrice = (target) => {
+  const productInfoDıv = target.closest(".product-info");
+  console.log(productInfoDıv);
+};
+
+const calculateCartPrice = () => {};
